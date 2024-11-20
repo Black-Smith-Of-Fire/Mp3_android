f14d2b2f652840027ad3592a10cb331f06b9ac04
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-20 23:41:48 +0530

    The bottom menubar is functional

--- app/src/main/java/com/example/mp3android/MainActivity.java
+++ app/src/main/java/com/example/mp3android/MainActivity.java
@@ -4,16 +4,27 @@ import android.annotation.SuppressLint;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
 import android.media.MediaPlayer;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private BottomNavigationView bottomNav;
+    private FrameLayout frameLayout;
 
     @SuppressLint("MissingInflatedId")
     @Override
@@ -22,5 +33,41 @@ public class MainActivity extends AppCompatActivity {
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
 
+        bottomNav = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.home) {
+                    fragmentChanger(new HomeFragment(), false);
+                } else if (itemId == R.id.playlist) {
+                    fragmentChanger(new PlaylistFragment(), false);
+                } else {
+                    fragmentChanger(new SettingsFragment(),false);
+                }
+
+                return true;
+            }
+        });
+        fragmentChanger(new HomeFragment(), true);
+
+    }
+
+
+    private void fragmentChanger(Fragment fragment, boolean isAppInitialized) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.commit();
     }
 }

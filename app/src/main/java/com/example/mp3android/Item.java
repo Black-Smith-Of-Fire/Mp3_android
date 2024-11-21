2b95216e1c35831490cef84a30a88b95fc2b3149
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-21 18:39:27 +0530

    Class to contain the data

---
+++ app/src/main/java/com/example/mp3android/Item.java
@@ -0,0 +1,19 @@
+package com.example.mp3android;
+
+public class Item {
+    int image;
+
+    public Item(String name, int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+
+
+}

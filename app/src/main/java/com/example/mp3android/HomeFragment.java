204e25d460c6fe7e0c5b4745519d7f5db0bfe88f
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-21 21:34:33 +0530

    App is working

--- app/src/main/java/com/example/mp3android/HomeFragment.java
+++ app/src/main/java/com/example/mp3android/HomeFragment.java
@@ -3,17 +3,45 @@ package com.example.mp3android;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class HomeFragment extends Fragment {
 
-
+    private RecyclerView recyclerView;
+    private List<Item> items;
+    private MyAdapter adapter;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_home,container,false);
+
+         items = itemList();
+        //Recycler view
+        recyclerView = rootView.findViewById(R.id.recyclerView);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        adapter = new MyAdapter(items);
+        recyclerView.setAdapter(adapter);
+
         return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    private List<Item> itemList(){
+
+        List<Item> items = new ArrayList<Item>();
+        items.add(new Item(R.drawable.blacksmith));
+        return items;
     }
 }

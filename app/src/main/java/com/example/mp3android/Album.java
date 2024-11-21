fb099cddf5c1a1f6d50074f2705c6bde8b0c716e
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-22 02:41:35 +0530

    New activity up and running

--- app/src/main/java/com/example/mp3android/Album.java
+++ app/src/main/java/com/example/mp3android/Album.java
@@ -1,6 +1,7 @@
 package com.example.mp3android;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,11 +11,16 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class Album extends AppCompatActivity {
 
+    TextView lol ;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_album);
+
+        lol = findViewById(R.id.lol);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);

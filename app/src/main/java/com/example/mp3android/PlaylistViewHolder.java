df18af05b661bbf95190fca38213b7584f7a06bf
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-22 01:37:40 +0530

    Proper refactoring done

--- app/src/main/java/com/example/mp3android/PlaylistViewHolder.java
+++ app/src/main/java/com/example/mp3android/PlaylistViewHolder.java
@@ -1,4 +1,17 @@
 package com.example.mp3android;
 
-public class PlaylistViewHolder {
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class PlaylistViewHolder extends RecyclerView.ViewHolder {
+
+    ImageView imageView;
+
+    public PlaylistViewHolder(@NonNull View itemView) {
+        super(itemView);
+        imageView = itemView.findViewById(R.id.imageview);
+    }
 }

33a0eae8eaa396afb991fabcdefcad4e3f19888c
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-22 01:33:07 +0530

    An adapter class for the playlist

---
+++ app/src/main/java/com/example/mp3android/PlaylistAdapter.java
@@ -0,0 +1,36 @@
+package com.example.mp3android;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class PlaylistAdapter extends RecyclerView.Adapter<ArtistsViewHolder> {
+
+    private List<Item> items;
+
+    public PlaylistAdapter(List<Item> items) {
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public PlaylistViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.artists_view, parent,false);
+        return new PlaylistViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ArtistsViewHolder holder, int position) {
+        holder.imageView.setImageResource(items.get(position).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+}

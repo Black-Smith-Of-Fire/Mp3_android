b3b12d9b5e7fa1ce5e6c8552c62a7214fac11404
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-22 20:15:45 +0530

    Removed the intent

---
+++ app/src/main/java/com/example/mp3android/MainActivity.java
@@ -0,0 +1,95 @@
+package com.example.mp3android;
+
+import android.os.Bundle;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.media.MediaPlayer;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowInsets;
+import android.widget.Button;
+import android.widget.FrameLayout;
+
+import com.example.mp3android.artists.HomeFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.example.mp3android.playlist.*;
+import com.example.mp3android.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private BottomNavigationView bottomNav;
+    private FrameLayout frameLayout;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+
+        //hide the status bar
+        View decoreView = getWindow().getDecorView();
+        decoreView.setOnApplyWindowInsetsListener(new View.OnApplyWindowInsetsListener() {
+            @NonNull
+            @Override
+            public WindowInsets onApplyWindowInsets(@NonNull View v, @NonNull WindowInsets insets) {
+                int left = insets.getSystemWindowInsetLeft();
+                int top = insets.getSystemWindowInsetTop();
+                int right = insets.getSystemWindowInsetRight();
+                int bottom = insets.getSystemWindowInsetBottom();
+                v.setPadding(left,top,right,bottom);
+                return insets.consumeSystemWindowInsets();
+            }
+
+        });
+
+
+        bottomNav = findViewById(R.id.bottomNavView);
+        frameLayout = findViewById(R.id.frameLayout);
+
+        bottomNav.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+
+                int itemId = item.getItemId();
+
+                if (itemId == R.id.home) {
+                    fragmentChanger(new HomeFragment(), false);
+                } else if (itemId == R.id.playlist) {
+                    fragmentChanger(new PlaylistFragment(), false);
+                } else {
+                    fragmentChanger(new SettingsFragment(),false);
+                }
+
+                return true;
+            }
+        });
+        fragmentChanger(new HomeFragment(), true);
+
+    }
+
+    private void fragmentChanger(Fragment fragment, boolean isAppInitialized) {
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+
+        if (isAppInitialized) {
+            fragmentTransaction.add(R.id.frameLayout, fragment);
+        } else {
+            fragmentTransaction.replace(R.id.frameLayout, fragment);
+        }
+        fragmentTransaction.commit();
+    }
+
+}

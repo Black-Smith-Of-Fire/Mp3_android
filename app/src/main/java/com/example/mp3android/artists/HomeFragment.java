735b4dfbe910d60d905b85eae1c3e367d00b1186
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-22 19:00:33 +0530

    Added names in the list

--- app/src/main/java/com/example/mp3android/artists/HomeFragment.java
+++ app/src/main/java/com/example/mp3android/artists/HomeFragment.java
@@ -46,17 +46,17 @@ public class HomeFragment extends Fragment implements ArtistInterface {
 
     private List<Item> itemList() {
 
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
-        it.add(new Item(R.drawable.blacksmith));
+        it.add(new Item(R.drawable.blacksmith,"Chris"));
+        it.add(new Item(R.drawable.blacksmith,"Jaime"));
+        it.add(new Item(R.drawable.blacksmith,"Sara"));
+        it.add(new Item(R.drawable.blacksmith,"Napoleon"));
+        it.add(new Item(R.drawable.blacksmith,"Dj malone"));
+        it.add(new Item(R.drawable.blacksmith,"Indila"));
+        it.add(new Item(R.drawable.blacksmith,"Terry"));
+        it.add(new Item(R.drawable.blacksmith,"Davis"));
+        it.add(new Item(R.drawable.blacksmith,"Burger king"));
+        it.add(new Item(R.drawable.blacksmith,"Shomw"));
+        it.add(new Item(R.drawable.blacksmith,"Carmine"));
         return it;
     }
 

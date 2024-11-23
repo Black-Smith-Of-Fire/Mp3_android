5418bce2d954706c8f5b95c45ad0a3281d6af9d8
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-23 22:41:57 +0530

    Included this.name

---
+++ app/src/main/java/com/example/mp3android/album/AlbumAdapter.java
@@ -0,0 +1,42 @@
+package com.example.mp3android.album;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mp3android.Item;
+import com.example.mp3android.album.AlbumViewHolder;
+import com.example.mp3android.R;
+
+import java.util.List;
+
+public class AlbumAdapter extends RecyclerView.Adapter<AlbumViewHolder> {
+
+    private List<Item> items;
+    private List<Item> names;
+
+    public AlbumAdapter(List<Item> items) {
+        this.items = items;
+        this.names = names;
+    }
+
+    @NonNull
+    @Override
+    public AlbumViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.songlist_view, parent,false);
+        return new AlbumViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull AlbumViewHolder holder, int position) {
+        holder.imageView.setImageResource(items.get(position).getImage());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
+}

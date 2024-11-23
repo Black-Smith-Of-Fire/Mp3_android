4cdb585561f9b2b91cf075bcdf39c48706b0bdc6
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-23 23:53:43 +0530

    Included the ArtistInterface

--- app/src/main/java/com/example/mp3android/album/AlbumViewHolder.java
+++ app/src/main/java/com/example/mp3android/album/AlbumViewHolder.java
@@ -7,13 +7,26 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mp3android.R;
+import com.example.mp3android.artists.ArtistInterface;
 
 public class AlbumViewHolder extends RecyclerView.ViewHolder {
 
     public ImageView imageView;
 
-    public AlbumViewHolder(@NonNull View itemView) {
+    public AlbumViewHolder(@NonNull View itemView, ArtistInterface artistInterface) {
         super(itemView);
         imageView = itemView.findViewById(R.id.profilePic);
+
+        itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (artistInterface != null) {
+                    int pos = getAdapterPosition();
+                    if (pos != RecyclerView.NO_POSITION) {
+                        artistInterface.onItemClick(pos);
+                    }
+                }
+            }
+        });
     }
 }

93030db0ec736960f91c1892ed01107e4d2dddc1
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-26 22:21:36 +0530

    Refactored lol to music

--- app/src/main/java/com/example/mp3android/player/PlayerActivity.java
+++ app/src/main/java/com/example/mp3android/player/PlayerActivity.java
@@ -36,10 +36,10 @@ public class PlayerActivity extends AppCompatActivity {
 
         TextView artistName = findViewById(R.id.playText);
 
-        int lol = R.raw.twistedrock;
+        int music = R.raw.twistedrock;
 
         // Media code
-        mediaPlayer = MediaPlayer.create(this,lol);
+        mediaPlayer = MediaPlayer.create(this,music);
         play = (Button)findViewById(R.id.play);
         forward = (Button)findViewById(R.id.forward);
         back = (Button)findViewById(R.id.rewind);
@@ -94,23 +94,6 @@ public class PlayerActivity extends AppCompatActivity {
                 }
             }
         }.start();
-    }
-
-    private List<Item> itemList() {
-
-        List<Item> it = new ArrayList<>();
-        it.add(new Item(R.drawable.blacksmith,"Chris"));
-        it.add(new Item(R.drawable.blacksmith,"Jaime"));
-        it.add(new Item(R.drawable.blacksmith,"Sara"));
-        it.add(new Item(R.drawable.blacksmith,"Napoleon"));
-        it.add(new Item(R.drawable.blacksmith,"Dj malone"));
-        it.add(new Item(R.drawable.blacksmith,"Indila"));
-        it.add(new Item(R.drawable.blacksmith,"Terry"));
-        it.add(new Item(R.drawable.blacksmith,"Davis"));
-        it.add(new Item(R.drawable.blacksmith,"Burger king"));
-        it.add(new Item(R.drawable.blacksmith,"Shomw"));
-        it.add(new Item(R.drawable.blacksmith,"Carmine"));
-        return it;
     }
 
 }

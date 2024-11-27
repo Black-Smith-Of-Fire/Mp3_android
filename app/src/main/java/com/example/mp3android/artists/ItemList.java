e3966d3658fdd06d9c4e2f7fecedaf5143ffad88
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-28 00:16:03 +0530

    Implemented database integration unsuccessfully

--- app/src/main/java/com/example/mp3android/artists/ItemList.java
+++ app/src/main/java/com/example/mp3android/artists/ItemList.java
@@ -3,26 +3,43 @@ package com.example.mp3android.artists;
 import com.example.mp3android.Item;
 import com.example.mp3android.R;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ItemList {
+    private String url = "jdbc:mysql://127.0.0.1:3306/mp3android";
+    private String user = "ben";
+    private String password = "example-password";
 
     List<Item> it = new ArrayList<>();
 
-    public List<Item> itemList() {
+public List<Item> itemList() {
 
-        it.add(new Item(R.drawable.blacksmith,"Chris"));
-        it.add(new Item(R.drawable.blacksmith,"Jaime"));
-        it.add(new Item(R.drawable.blacksmith,"Sara"));
-        it.add(new Item(R.drawable.blacksmith,"Napoleon"));
-        it.add(new Item(R.drawable.blacksmith,"Dj malone"));
-        it.add(new Item(R.drawable.blacksmith,"Indila"));
-        it.add(new Item(R.drawable.blacksmith,"Terry"));
-        it.add(new Item(R.drawable.blacksmith,"Davis"));
-        it.add(new Item(R.drawable.blacksmith,"Burger king"));
-        it.add(new Item(R.drawable.blacksmith,"Shomw"));
-        it.add(new Item(R.drawable.blacksmith,"Carmine"));
+    try{
+        Connection connection = DriverManager.getConnection(url,user,password);
+        Statement statement = connection.createStatement();
+
+        String query = "Select * from artistList";
+
+        statement.executeQuery(query);
+
+        ResultSet resultSet = statement.getResultSet();
+
+        while(resultSet.next()){
+            it.add(new Item(R.drawable.blacksmith,resultSet.getString("artistName")));
+        }
+
+    } catch (SQLException e) {
+        throw new RuntimeException(e);
+    }
+
+//        it.add(new Item(R.drawable.blacksmith,"Shomw"));
+//        it.add(new Item(R.drawable.blacksmith,"Carmine"));
         return it;
     }
 }

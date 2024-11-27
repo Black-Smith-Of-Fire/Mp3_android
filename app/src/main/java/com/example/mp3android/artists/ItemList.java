6aa33a6a3a1f58246c059241499348f5afb41629
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-27 23:57:56 +0530

    Class for database management

---
+++ app/src/main/java/com/example/mp3android/artists/ItemList.java
@@ -0,0 +1,28 @@
+package com.example.mp3android.artists;
+
+import com.example.mp3android.Item;
+import com.example.mp3android.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ItemList {
+
+    List<Item> it = new ArrayList<>();
+
+    private List<Item> itemList() {
+
+        it.add(new Item(R.drawable.blacksmith,"Chris"));
+        it.add(new Item(R.drawable.blacksmith,"Jaime"));
+        it.add(new Item(R.drawable.blacksmith,"Sara"));
+        it.add(new Item(R.drawable.blacksmith,"Napoleon"));
+        it.add(new Item(R.drawable.blacksmith,"Dj malone"));
+        it.add(new Item(R.drawable.blacksmith,"Indila"));
+        it.add(new Item(R.drawable.blacksmith,"Terry"));
+        it.add(new Item(R.drawable.blacksmith,"Davis"));
+        it.add(new Item(R.drawable.blacksmith,"Burger king"));
+        it.add(new Item(R.drawable.blacksmith,"Shomw"));
+        it.add(new Item(R.drawable.blacksmith,"Carmine"));
+        return it;
+    }
+}

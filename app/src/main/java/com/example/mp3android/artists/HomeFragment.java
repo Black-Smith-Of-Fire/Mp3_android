1faf1800463f7acca2296732e24de2e061c82e27
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-27 23:59:49 +0530

    Class working

--- app/src/main/java/com/example/mp3android/artists/HomeFragment.java
+++ app/src/main/java/com/example/mp3android/artists/HomeFragment.java
@@ -27,15 +27,14 @@ public class HomeFragment extends Fragment implements ArtistInterface {
     private RecyclerView recyclerView;
     private List<Item> items;
     private ArtistsAdapter adapter;
-    List<Item> it = new ArrayList<>();
-
+    private ItemList item = new ItemList();
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_home,container,false);
 
-         items = itemList();
+         items = item.itemList();
         //Recycler view
         recyclerView = rootView.findViewById(R.id.recyclerView);
 
@@ -47,21 +46,6 @@ public class HomeFragment extends Fragment implements ArtistInterface {
         return rootView;
     }
 
-    private List<Item> itemList() {
-
-        it.add(new Item(R.drawable.blacksmith,"Chris"));
-        it.add(new Item(R.drawable.blacksmith,"Jaime"));
-        it.add(new Item(R.drawable.blacksmith,"Sara"));
-        it.add(new Item(R.drawable.blacksmith,"Napoleon"));
-        it.add(new Item(R.drawable.blacksmith,"Dj malone"));
-        it.add(new Item(R.drawable.blacksmith,"Indila"));
-        it.add(new Item(R.drawable.blacksmith,"Terry"));
-        it.add(new Item(R.drawable.blacksmith,"Davis"));
-        it.add(new Item(R.drawable.blacksmith,"Burger king"));
-        it.add(new Item(R.drawable.blacksmith,"Shomw"));
-        it.add(new Item(R.drawable.blacksmith,"Carmine"));
-        return it;
-    }
 
     @Override
     public void onItemClick(int position) {

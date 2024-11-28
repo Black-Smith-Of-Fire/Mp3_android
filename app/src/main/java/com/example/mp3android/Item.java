b32edc2267301b0a69702ce8a94490b2e2a395c4
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-28 22:06:37 +0530

    Getter and setter for music

--- app/src/main/java/com/example/mp3android/Item.java
+++ app/src/main/java/com/example/mp3android/Item.java
@@ -3,12 +3,18 @@ package com.example.mp3android;
 public class Item {
     String name;
     int image;
+    int music;
 
     public Item(int image, String name) {
         this.image = image;
         this.name = name;
     }
 
+    public Item(int image, String name, int music) {
+        this.image = image;
+        this.name = name;
+        this.music = music;
+    }
     public String getName() {
         return name;
     }
@@ -26,5 +32,12 @@ public class Item {
         this.image = image;
     }
 
+    public int getMusic() {
+        return music;
+    }
+
+    public void setMusic(int music) {
+        this.music = music;
+    }
 
 }

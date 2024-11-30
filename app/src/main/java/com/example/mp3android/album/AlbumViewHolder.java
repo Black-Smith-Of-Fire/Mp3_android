7c9e780854c2c64f71a737c596a0659b323cdb4d
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-11-30 18:30:08 +0530

    Instantiated textview songName and artist

--- app/src/main/java/com/example/mp3android/album/AlbumViewHolder.java
+++ app/src/main/java/com/example/mp3android/album/AlbumViewHolder.java
@@ -2,6 +2,7 @@ package com.example.mp3android.album;
 
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
@@ -12,10 +13,13 @@ import com.example.mp3android.artists.ArtistInterface;
 public class AlbumViewHolder extends RecyclerView.ViewHolder {
 
     public ImageView imageView;
+    public TextView songName, artist;
 
     public AlbumViewHolder(@NonNull View itemView, ArtistInterface artistInterface) {
         super(itemView);
         imageView = itemView.findViewById(R.id.profilePic);
+        songName = itemView.findViewById(R.id.songName);
+        artist = itemView.findViewById(R.id.artist);
 
         itemView.setOnClickListener(new View.OnClickListener() {
             @Override

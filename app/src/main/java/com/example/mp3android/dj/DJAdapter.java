bc76642440249d5d9d5827a3aa8b875d71e00c96
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-05 15:42:12 +0530

    Adapter class for dj is ready

--- app/src/main/java/com/example/mp3android/dj/DJAdapter.java
+++ app/src/main/java/com/example/mp3android/dj/DJAdapter.java
@@ -1,4 +1,41 @@
 package com.example.mp3android.dj;
 
-public class DJAdapter {
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.mp3android.Item;
+import com.example.mp3android.R;
+
+import java.util.List;
+
+public class DJAdapter extends RecyclerView.Adapter<DJViewHolder> {
+
+    private List<Item> items;
+
+    public DJAdapter(List<Item> items){
+        this.items = items;
+    }
+
+    @NonNull
+    @Override
+    public DJViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.dj_list, parent,false);
+        return new DJViewHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull DJViewHolder holder, int position) {
+        holder.imageView.setImageResource(items.get(position).getImage());
+        holder.songName.setText(items.get(position).getName());
+        holder.artist.setText(items.get(position).getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return items.size();
+    }
 }

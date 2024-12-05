53b43678baab8aae0d77eea3dba0866f262eac86
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-05 12:32:03 +0530

    Refactored Settings to DJ

--- app/src/main/java/com/example/mp3android/DJFragment.java
+++ app/src/main/java/com/example/mp3android/DJFragment.java
@@ -7,13 +7,13 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-public class SettingsFragment extends Fragment {
+public class DJFragment extends Fragment {
 
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_settings, container, false);
+        return inflater.inflate(R.layout.fragment_dj, container, false);
     }
 }

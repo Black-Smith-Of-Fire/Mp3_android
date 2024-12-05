206347917630afde2d8973b6cad6e999774b9f86
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-05 15:43:07 +0530

    New songs for the mix is showing

--- app/src/main/java/com/example/mp3android/dj/DJFragment.java
+++ app/src/main/java/com/example/mp3android/dj/DJFragment.java
@@ -3,17 +3,24 @@ package com.example.mp3android.dj;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.mp3android.Item;
 import com.example.mp3android.R;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DJFragment extends Fragment {
 
     private RecyclerView recyclerView;
+    private List<Item> items;
+    private DJAdapter adapter;
 
 
     @Override
@@ -22,8 +29,22 @@ public class DJFragment extends Fragment {
 
         View rootView = inflater.inflate(R.layout.fragment_dj, container,false);
 
+        items = itemList();
         recyclerView = rootView.findViewById(R.id.djRecyclerView);
 
-        return inflater.inflate(R.layout.fragment_dj, container, false);
+        adapter = new DJAdapter(items);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setAdapter(adapter);
+
+        return rootView;
+    }
+
+    private List<Item> itemList() {
+
+        List<Item> it = new ArrayList<>();
+        it.add(new Item(R.drawable.blacksmith,"Chris"));
+        it.add(new Item(R.drawable.blacksmith,"Burger king"));
+        it.add(new Item(R.drawable.blacksmith,"Carmine"));
+        return it;
     }
 }

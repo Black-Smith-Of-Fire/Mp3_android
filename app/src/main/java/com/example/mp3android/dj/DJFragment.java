23dc576cb8bc6b05b13cb60902deab72986372d3
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-05 14:50:00 +0530

    Moved the DJ fragment to a package of its own

--- app/src/main/java/com/example/mp3android/dj/DJFragment.java
+++ app/src/main/java/com/example/mp3android/dj/DJFragment.java
@@ -1,19 +1,29 @@
-package com.example.mp3android;
+package com.example.mp3android.dj;
 
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.example.mp3android.R;
+
 public class DJFragment extends Fragment {
 
+    private RecyclerView recyclerView;
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View rootView = inflater.inflate(R.layout.fragment_dj, container,false);
+
+        recyclerView = rootView.findViewById(R.id.djRecyclerView);
+
         return inflater.inflate(R.layout.fragment_dj, container, false);
     }
 }

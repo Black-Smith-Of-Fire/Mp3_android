da8fe6d2eb935a6777db54c4b03b4b096e2f3480
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-06 16:00:12 +0530

    Play button is working, so music is playing and stopping on click

--- app/src/main/java/com/example/mp3android/dj/DJViewHolder.java
+++ app/src/main/java/com/example/mp3android/dj/DJViewHolder.java
@@ -1,8 +1,11 @@
 package com.example.mp3android.dj;
 
 import android.content.Context;
+import android.media.MediaPlayer;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -22,13 +25,18 @@ public class DJViewHolder extends RecyclerView.ViewHolder {
     public ImageView imageView;
     public TextView songName, artist;
     public RangeSlider rangeSlider;
+    public ImageButton play;
+    MediaPlayer mediaPlayer;
 
     public DJViewHolder(@NonNull View itemView) {
         super(itemView);
+
         imageView = itemView.findViewById(R.id.artistImage1);
         songName = itemView.findViewById(R.id.songName1);
         artist = itemView.findViewById(R.id.artist2);
         rangeSlider = itemView.findViewById(R.id.slider);
+        play = (ImageButton) itemView.findViewById(R.id.play);
+        mediaPlayer = MediaPlayer.create(this.itemView.getContext(), R.raw.twistedrock);
 
 //        rangeSlider.addOnSliderTouchListener(new RangeSlider.OnSliderTouchListener() {
 //            @Override
@@ -49,6 +57,25 @@ public class DJViewHolder extends RecyclerView.ViewHolder {
                 List <Float> list = rangeSlider.getValues();
                 Toast.makeText(itemView.getContext(),"X: " + list.get(0) ,Toast.LENGTH_SHORT).show();
                 Toast.makeText(itemView.getContext(),"Y: " + list.get(1) ,Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int music = R.raw.twistedrock;
+
+                if(!mediaPlayer.isPlaying()) { // start the  music
+                    play.setBackgroundResource(R.drawable.baseline_pause_24);
+//                    play.setBackground(getResources().getDrawable(R.drawable.round_button));
+//                    play.setImageResource(R.drawable.round_button);
+                    mediaPlayer.start();
+                }
+                else { // stop it
+                    play.setBackground(null);
+                    play.setBackgroundResource(R.drawable.baseline_play_arrow_24);
+                    mediaPlayer.pause();
+                }
             }
         });
     }

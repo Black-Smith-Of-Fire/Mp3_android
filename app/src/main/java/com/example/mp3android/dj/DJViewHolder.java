0744a48cb2889036101d8ea48c70a818ee921421
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-06 14:57:05 +0530

    Implemented the listener for the rangesliders

--- app/src/main/java/com/example/mp3android/dj/DJViewHolder.java
+++ app/src/main/java/com/example/mp3android/dj/DJViewHolder.java
@@ -1,23 +1,55 @@
 package com.example.mp3android.dj;
 
+import android.content.Context;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.mp3android.R;
+import com.google.android.material.slider.RangeSlider;
+import com.google.android.material.slider.Slider;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class DJViewHolder extends RecyclerView.ViewHolder {
 
     public ImageView imageView;
     public TextView songName, artist;
+    public RangeSlider rangeSlider;
 
     public DJViewHolder(@NonNull View itemView) {
         super(itemView);
         imageView = itemView.findViewById(R.id.artistImage1);
         songName = itemView.findViewById(R.id.songName1);
         artist = itemView.findViewById(R.id.artist2);
+        rangeSlider = itemView.findViewById(R.id.slider);
+
+//        rangeSlider.addOnSliderTouchListener(new RangeSlider.OnSliderTouchListener() {
+//            @Override
+//            public void onStartTrackingTouch(@NonNull RangeSlider slider) {
+//                Log.i("Slider's X value is : ","X is working , hurray");
+//            }
+//
+//            @Override
+//            public void onStopTrackingTouch(@NonNull RangeSlider slider) {
+//                Log.i("Slider's Y value is : ", list.get(1).toString());
+//
+//            }
+//        });
+
+        rangeSlider.addOnChangeListener(new RangeSlider.OnChangeListener(){
+            @Override
+            public void onValueChange(@NonNull RangeSlider slider, float value , boolean fromUser){
+                List <Float> list = rangeSlider.getValues();
+                Toast.makeText(itemView.getContext(),"X: " + list.get(0) ,Toast.LENGTH_SHORT).show();
+                Toast.makeText(itemView.getContext(),"Y: " + list.get(1) ,Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }

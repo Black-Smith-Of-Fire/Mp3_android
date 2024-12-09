951396de33f2c11c99cbe8d12b9099885bd38764
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-09 19:25:35 +0530

    A class to switch between different music

---
+++ app/src/main/java/com/example/mp3android/dj/MusicHandler.java
@@ -0,0 +1,12 @@
+package com.example.mp3android.dj;
+
+public class MusicHandler {
+
+//    public void playMusic(){
+//        mediaPlayer.start();
+//    }
+//
+//    private boolean nextInline(boolean value){
+//        return value;
+//    }
+}

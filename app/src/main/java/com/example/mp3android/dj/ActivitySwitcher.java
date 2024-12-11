0800d157ca57b9dcc2309283bd8b2fb650d3f9c2
Black_Smith_Of_Fire <blacksmithoffire@gmail.com>
2024-12-12 02:29:00 +0530

    The new activity

--- app/src/main/java/com/example/mp3android/dj/ActivitySwitcher.java
+++ app/src/main/java/com/example/mp3android/dj/ActivitySwitcher.java
@@ -1,4 +1,17 @@
 package com.example.mp3android.dj;
 
+import android.media.MediaPlayer;
+
+import java.util.List;
+
 public class ActivitySwitcher {
+    MediaPlayer mediaPlayer;
+    DJItemList lol = new DJItemList();
+
+    ActivitySwitcher(int image, int songName, List<Float> items, MediaPlayer mediaPlayer){
+        this.mediaPlayer = mediaPlayer;
+        if (lol.itemList().get(0).getSwitchOnMusic()) {
+            mediaPlayer.start();
+        }
+    }
 }
